f263b40c51b6d54e2a5cb078dabf4804cde3ae18
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-03 14:28:28 +0600

    variables done with scope

---
+++ 01-basic/03-variables.py
@@ -0,0 +1,21 @@
+myvar = "myvar"
+my_var = "my_var"
+_my_var = "_my_var"
+myVar = "myVar"
+MYVAR = "MYVAR"
+myvar2 = "myvar2"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(MYVAR)
+print(myvar2)
+
+#One Value to Multiple Variables
+a = b = c = "One Value to Multiple Variables"
+# print(a)
+# print(b)
+# print(c)
+print(a, b, c)
+
+fruits = ["Mango", "Banana", "Apple"]
+print(fruits)
