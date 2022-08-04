363427c10537650c33b35b05675b645a794d0030
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-04 09:44:35 +0600

    added some necessary images

---
+++ 02-flow-control/01-if-statement.py
@@ -0,0 +1,8 @@
+#even odd checker
+num = 3
+if(num % 2 == 0):
+   print(num, "is the even number")
+elif (num % 2 == 1):
+   print(num, "is odd number")
+else:
+   print(num, "is neither odd nor even")
