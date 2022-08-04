0e2f54920b91bada4c5fe258de0b5a368cd5ac5c
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-04 10:09:27 +0600

    if else complete

--- 02-flow-control/01-if-statement.py
+++ 02-flow-control/01-if-statement.py
@@ -1,8 +1,5 @@
 #even odd checker
 num = 3
-if(num % 2 == 0):
-   print(num, "is the even number")
-elif (num % 2 == 1):
-   print(num, "is odd number")
-else:
-   print(num, "is neither odd nor even")
+if(num > 5):
+   print(num, "is less than 3")
+print("It is not in if")
---
+++ 02-flow-control/02-if-else-statement.py
@@ -0,0 +1,13 @@
+num = 20
+if(num < 15):
+   print(num, "is smaller than 15")
+else:
+   print(num, "is greater than 15")
+
+if(num == 20):
+   if(num < 25):
+      print(num, "is smaller than 25")
+   if(num < 15):
+      print(num, "is greater than 15")
+   else:
+      print(num, "is greater than 15")
