8e5ee925861fa71e0b9bdfca6b2979e33f9a88af
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-11 08:44:47 +0600

    constructor overloading done

--- 04-functions/03-two-star-args.py
+++ 04-functions/03-two-star-args.py
@@ -1,18 +1,18 @@
 # using **arg argument, it will receive a dictionary of arguments
 
-def myFunction(country="Bangladesh"):
+def my_function(country="Bangladesh"):
     print("I am from ", country)
 
 
-myFunction("USA")
-myFunction()
-myFunction("UK")
-myFunction("Pakistan")
+my_function("USA")
+my_function()
+my_function("UK")
+my_function("Pakistan")
 
 
 # using the **agrs that is received the dictionary
-def myArgumentFunction(**student):
+def my_argument_function(**student):
     print("Father name ", student["father"])
 
 
-myArgumentFunction(name="John", father="sammy Ahmed")
+my_argument_function(name="John", father="sammy Ahmed")
