7a770fd34c419ca217b258ca59fdd8014306301e
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-12 11:20:45 +0600

    static or class variable done

--- 06-objec-class/05-four-pillars-eipa/02-inheritance/01-inheritance.py
+++ 06-objec-class/05-four-pillars-eipa/02-inheritance/01-inheritance.py
@@ -1,22 +1,22 @@
-def flyParent():
+def fly_parent():
     print('I can fly independently > parent class')
 
 
-class birdParent:
+class BirdParent:
     def __init__(self):
         print('This is parent of bird > parent class')
 
 
-def flyChild():
+def fly_child():
     print('I do not fly independently > child class')
 
 
-class birdChild(birdParent):
+class BirdChild(BirdParent):
     def __init__(self):
         super().__init__()  # call super function
         print('This is child bird > child class')
 
 
-myObj = birdChild()
-flyParent()
-flyChild()
+myObj = BirdChild()
+fly_parent()
+fly_child()
