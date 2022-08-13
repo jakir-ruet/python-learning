a2566214a060f60df45a1305d3e6d3f43995242f
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-13 08:32:28 +0600

    one ex of class methhod added

--- 06-objec-class/06-methods/04-static-method.py
+++ 06-objec-class/06-methods/04-static-method.py
@@ -1,8 +1,25 @@
 class Employee:
-    @staticmethod
-    def detail(x):
-        print('This is an Employee', x)
+    edu_ins_name = 'RUET'
+
+    def __init__(self, name, age):
+        self.Name = name
+        self.Age = age
+
+    def details(self):
+        print(self.Name, self.Age)
+
+    @classmethod
+    def add_edu_ins(cls, ins_name):
+        cls.add_edu_ins = ins_name
+
+    @classmethod
+    def input_from_string(cls, string_name):
+        name, age = string_name.split('-')
+        obj = cls(name, age)
+        return obj
 
 
-# call static method
-Employee.detail(10)
+emp1 = Employee('Jakir', 35)
+emp1.details()
+emp2 = Employee.input_from_string('Jasim-45')
+emp2.details()
--- 06-objec-class/06-methods/05-static-method.py
+++ 06-objec-class/06-methods/05-static-method.py
@@ -1,43 +1,8 @@
-class Employee(object):
-    def __init__(self, name, salary, project):
-        self.Name = name
-        self.Salary = salary
-        self.Project = project
-
+class Employee:
     @staticmethod
-    def details(project):
-        if project == 'Console Project':
-            requirement = ['C', 'Compiler', 'MinGW']
-        else:
-            requirement = ['C']
-        return requirement
-
-    # instance method
-    def work(self):
-        # call static method from instance method
-        requirement = self.details(self.Project)
-        for req in requirement:
-            print('Completed', req)
+    def detail(x):
+        print('This is an Employee', x)
 
 
-emp = Employee('Jakir', 3500, 'Console Project')
-emp.work()
-
-
-# Call Static Method from Another Method
-class Test:
-    @staticmethod
-    def static_method_1():
-        print('static method 1')
-
-    @staticmethod
-    def static_method_2():
-        Test.static_method_1()
-
-    @classmethod
-    def class_method_1(cls):
-        cls.static_method_2()
-
-
-# call class method
-Test.class_method_1()
+# call static method
+Employee.detail(10)
