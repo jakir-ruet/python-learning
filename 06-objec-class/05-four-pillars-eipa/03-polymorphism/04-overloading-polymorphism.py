72f36894cfc5310621f0d61d96bb305bbb3b1f06
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-14 08:43:11 +0600

    polymorphism with method overloading done

--- 06-objec-class/05-four-pillars-eipa/03-polymorphism/04-overloading-polymorphism.py
+++ 06-objec-class/05-four-pillars-eipa/03-polymorphism/04-overloading-polymorphism.py
@@ -1,6 +1,6 @@
-def addition(a, b):
-    c = a + b
-    print(c)
+# def addition(a, b):
+#     c = a + b
+#     print(c)
 
 
 def addition(a, b, c):
@@ -9,7 +9,7 @@ def addition(a, b, c):
 
 
 # the below line shows an error
-addition(4, 5)
+# addition(4, 5)
 
 # This line will call the second product method
 addition(3, 7, 5)
