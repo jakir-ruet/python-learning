8fa70989342b826d98d8ffc191c4ad05cb0ad82f
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-09-02 00:07:47 +0600

    stack done

---
+++ 08-dsa/01-stack/02-stack.py
@@ -0,0 +1,13 @@
+list=[]
+list.append(9)
+print("push:",list)
+list.append(10)
+print("push:",list)
+list.append(11)
+print("push:",list)
+list.pop() # pop 10
+print("pop:",list)
+print("peek:",list[-1]) # get top most element
+list.pop() # pop 10
+print("pop:",list)
+print("peek:",list[-1]) # get top most element
