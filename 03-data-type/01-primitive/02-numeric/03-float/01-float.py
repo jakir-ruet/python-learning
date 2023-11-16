cc2a89a152454d37e7e2fb646cad81ee0ff66ebe
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-16 11:43:44 +0600

    float number done

--- 03-data-type/01-primitive/02-numeric/03-float/01-float.py
+++ 03-data-type/01-primitive/02-numeric/03-float/01-float.py
@@ -1,2 +1,16 @@
-b = 50.05
+a = 50.05
+print(a, "is  of type", type(a))
+
+b = 0.05
 print(b, "is  of type", type(b))
+
+c = -50.05
+print(c, "is  of type", type(c))
+
+x = 35e3
+y = 12e4
+z = -87.7e100
+
+print(x, "is  of type", type(x))
+print(y, "is  of type", type(y))
+print(z, "is  of type", type(z))
