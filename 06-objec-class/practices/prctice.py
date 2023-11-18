c6e81ecbb2e2abb51aff425004e8492f96a59146
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-18 16:25:17 +0600

    practice is going on

--- 06-objec-class/practices/prctice.py
+++ 06-objec-class/practices/prctice.py
@@ -1,5 +1,6 @@
 class studentInfo:
     def __init__(self, student_id, student_name, class_name):
+        print(self, "is ram location")
         self.Student_Id = student_id
         self.Student_Name = student_name
         self.Class_Name = class_name
@@ -13,6 +14,8 @@ class studentInfo:
 
 
 studentObject = studentInfo(87, "Jakir", 11)
+studentObject1 = studentInfo(78, "Jasim", 12)
+print(studentObject, "is ram location")
 studentObject.show()
 studentObject.update("Jasim", 12)
 studentObject.show()
