a2eb1a927ef4ab0b66050a38c359581b4bd2d09f
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-24 19:25:31 +0600

    match case update

--- 02-flow-control/practice/04-match-case.py
+++ 02-flow-control/practice/04-match-case.py
@@ -1,13 +1,17 @@
 todos = []
 while True:
-    user_action = input("Type add, show, edit, exit ").strip()
+    user_action = input(
+        "Enter command (add, show, showall, edit, exit) ").strip()
     match user_action:
         case "add":
             todo = input("add here: ")
             todos.append(todo)
-        case "show":
+        case "showall":
             for i, item in enumerate(todos, start=1):
                 print(i, item)
+        case "show":
+            i = int(input("serial no: ")) - 1
+            print(i + 1, todos[i])
         case "edit":
             i = int(input("edit index: ")) - 1
             todo = input("edit with: ")
